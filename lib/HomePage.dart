fc32df54647eb8c6c89e28719db2cba3cf26b270
Yazdani <yaz4dani@gmail.com>
2019-04-10 00:41:17 +0600

    insert data

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -84,9 +84,7 @@ class _HomeState extends State<Home> {
                   );
                 }),
       ),
-
-
-
+      
       floatingActionButton: new FloatingActionButton(
           onPressed: (){
             Navigator.of(context).push(new MaterialPageRoute(builder: (context)=>addPost()));
